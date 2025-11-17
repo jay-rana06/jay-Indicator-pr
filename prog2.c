80d8353160e3d7c0f2c21dc93b934a0c7a68c878
jay-rana06 <jay257733@gmail.com>
2025-11-17 17:15:21 +0530

    update

---
+++ prog2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int *ptr;
+    int a = 10;
+
+    ptr = &a;
+
+    printf("address of a: %d",ptr);
+    printf("\n a is: %d",a);
+    printf("\n a is: %d",*ptr);
+    return 0;
+}
